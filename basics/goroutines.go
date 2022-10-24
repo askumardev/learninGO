6b9125dbe50195efd9dcb7d6886ac6c0a5304813
askumardev <askumar.dev@outlook.com>
2022-10-24 19:22:27 +0530

    go routines

---
+++ basics/goroutines.go
@@ -0,0 +1,28 @@
+// go run basics/goroutines.go
+
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func f(from string) {
+  for i := 0; i < 3; i++ {
+    fmt.Println(from, ":", i)
+  }
+}
+
+func main() {
+
+  f("direct")
+
+  go f("goroutine")
+
+  go func(msg string) {
+    fmt.Println(msg)
+  }("going")
+
+  time.Sleep(time.Second)
+  fmt.Println("done")
+}
