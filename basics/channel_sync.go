94221865d62b67f0c63c7278d99196281ffda22f
askumardev <askumar.dev@outlook.com>
2022-10-25 21:10:57 +0530

    channel_sync

---
+++ basics/channel_sync.go
@@ -0,0 +1,25 @@
+//Channel Synchronization
+// go run basics/channel_sync.go
+
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func worker(done chan bool) {
+  fmt.Print("working...")
+  time.Sleep(time.Second)
+  fmt.Println("done")
+
+  done <- true
+}
+
+func main() {
+
+  done := make(chan bool, 1)
+  go worker(done)
+
+  <-done
+}
