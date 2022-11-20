8f4c6c58982b895ea68ce4210f09e2c37038d145
askumardev <askumar.dev@outlook.com>
2022-11-20 09:24:15 +0530

    env vars

---
+++ adv/env_vars.go
@@ -0,0 +1,23 @@
+// go run adv/env_vars.go
+// BAR=2 go run adv/env_vars.go
+
+package main
+
+import (
+    "fmt"
+    "os"
+    "strings"
+)
+
+func main() {
+
+    os.Setenv("FOO", "1")
+    fmt.Println("FOO:", os.Getenv("FOO"))
+    fmt.Println("BAR:", os.Getenv("BAR"))
+
+    fmt.Println()
+    for _, e := range os.Environ() {
+        pair := strings.SplitN(e, "=", 2)
+        fmt.Println(pair[0])
+    }
+}
